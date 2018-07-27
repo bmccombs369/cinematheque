26e53ecf155e99972f57df1a7037266f365cceaf
bmccombs369 <bmccombs369@gmail.com>
2018-07-27 12:23:32 -0400

    movie posters showing

--- client/src/components/NewReleases.js
+++ client/src/components/NewReleases.js
@@ -12,23 +12,35 @@ class NewReleases extends Component {
 
   fetchNewReleases = async () => {
     try {
-    let date = new Date();
-    let today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-    let oneMonthAgo = (date.getMonth() === 0 ? date.getFullYear() - 1 + '-' + 12 + '-' + date.getDate() : date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate());
-    const apiKey = '9a25e1555b7eca6261c39235272224a8'
-    const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${oneMonthAgo}&primary_release_date.lte=${today}`);
-    this.setState({
-      newReleases: response.data.results
-    })
-  } catch (error) {
-    console.error(error)
-  }
+      let date = new Date();
+      let today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+      let oneMonthAgo = (date.getMonth() === 0 ? date.getFullYear() - 1 + '-' + 12 + '-' + date.getDate() : date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate());
+      const apiKey = '9a25e1555b7eca6261c39235272224a8'
+      const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${oneMonthAgo}&primary_release_date.lte=${today}`);
+      this.setState({
+        newReleases: response.data.results
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   render() {
     return (
       <div>
-        
+        <h2>New Releases</h2>
+        <div>
+          {this.state.newReleases.map((newMovie, i) => {
+            return (
+              <img
+                src={newMovie.poster_path
+                  ? `https://image.tmdb.org/t/p/w342${newMovie.poster_path}`
+                  : 'http://via.placeholder.com/342x513'}
+                alt={`${newMovie.title} poster`}
+              />
+            )
+          })}
+        </div>
       </div>
     );
   }
