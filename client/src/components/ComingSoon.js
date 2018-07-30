d4d7580a8d6cd0cc5f8d3299cbe543a80ebf9003
bmccombs369 <bmccombs369@gmail.com>
2018-07-30 12:04:53 -0400

    Coming Soon Page working

---
+++ client/src/components/ComingSoon.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class ComingSoon extends Component {
+  state = {
+    comingSoon: []
+  }
+
+  componentDidMount() {
+    this.fetchComingSoon();
+  }
+
+  fetchComingSoon = async () => {
+    try {
+      let date = new Date();
+      let today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+      let inOneMonth = (date.getMonth() === 11 ? `${date.getFullYear() + 1}-1-${date.getDate()}` : `${date.getFullYear()}-${date.getMonth() + 2}-${date.getDate()}`);
+      const apiKey = '9a25e1555b7eca6261c39235272224a8'
+      const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${today}&primary_release_date.lte=${inOneMonth}`);
+      this.setState({
+        comingSoon: response.data.results
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Coming Soon</h2>
+        <div>
+          {this.state.comingSoon.map((newMovie, i) => {
+            return (
+              <img
+                src={newMovie.poster_path
+                  ? `https://image.tmdb.org/t/p/w342${newMovie.poster_path}`
+                  : 'http://via.placeholder.com/342x513'}
+                alt={`${newMovie.title} poster`}
+              />
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ComingSoon;
