7f9878866c0a8c02eedabefccf4dae2898108972
bmccombs369 <bmccombs369@gmail.com>
2018-07-30 15:09:56 -0400

    created IndividualMovie component

---
+++ client/src/components/IndividualMovie.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class IndividualMovie extends Component {
+  state = {
+    movie: {}
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default IndividualMovie;
