1a6d112a54e95e3225e5ed220cb94a9b719d6946
bmccombs369 <bmccombs369@gmail.com>
2018-07-30 12:23:57 -0400

    NewReleases now returns in order of popularity and only english language

--- client/src/components/NewReleases.js
+++ client/src/components/NewReleases.js
@@ -16,7 +16,7 @@ class NewReleases extends Component {
       let today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
       let oneMonthAgo = (date.getMonth() === 0 ? `${date.getFullYear() - 1}-12-${date.getDate()}` : `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
       const apiKey = '9a25e1555b7eca6261c39235272224a8'
-      const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${oneMonthAgo}&primary_release_date.lte=${today}`);
+      const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${oneMonthAgo}&primary_release_date.lte=${today}&with_original_language=en`);
       this.setState({
         newReleases: response.data.results
       })
