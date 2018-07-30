5b8365d74bf8c776e2df1d7a069fb64e7d934246
bmccombs369 <bmccombs369@gmail.com>
2018-07-30 12:22:25 -0400

    coming soon now returns in order of popularity and only english language

--- client/src/components/ComingSoon.js
+++ client/src/components/ComingSoon.js
@@ -16,7 +16,7 @@ class ComingSoon extends Component {
       let today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
       let inOneMonth = (date.getMonth() === 11 ? `${date.getFullYear() + 1}-1-${date.getDate()}` : `${date.getFullYear()}-${date.getMonth() + 2}-${date.getDate()}`);
       const apiKey = '9a25e1555b7eca6261c39235272224a8'
-      const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${today}&primary_release_date.lte=${inOneMonth}`);
+      const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${today}&primary_release_date.lte=${inOneMonth}&with_original_language=en`);
       this.setState({
         comingSoon: response.data.results
       })
@@ -33,6 +33,7 @@ class ComingSoon extends Component {
           {this.state.comingSoon.map((newMovie, i) => {
             return (
               <img
+                key={i}
                 src={newMovie.poster_path
                   ? `https://image.tmdb.org/t/p/w342${newMovie.poster_path}`
                   : 'http://via.placeholder.com/342x513'}
