21a27c044e4d8c68038d7fcc49fb2badd5544fea
bmccombs369 <bmccombs369@gmail.com>
2018-08-03 09:58:58 -0400

    created lists index

---
+++ client/src/components/ListsIndex.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ListsIndex extends Component {
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default ListsIndex;
