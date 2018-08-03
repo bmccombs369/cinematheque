ac535480fc989f1bdff604084dfd23acebeb33b9
bmccombs369 <bmccombs369@gmail.com>
2018-08-03 09:45:11 -0400

    seeds working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,27 @@
 List.destroy_all
 Movie.destroy_all
 
-friday_thirteen = List.create!({
+friday_thirteen = List.create!(
   name: 'Friday the 13th',
   description: 'I went down to the basement, drew a pentagram, and summoned the best horror movies to watch the next time you need a spook.'
-})
+)
 
-Movie.create!({
-  movie_db_id: 948;
-})
+Movie.create!(
+  movie_db_id: 948,
+  list_id: friday_thirteen.id
+)
 
-Movie.create!({
-  movie_db_id: 4488;
-})
+Movie.create!(
+  movie_db_id: 4488,
+  list_id: friday_thirteen.id
+)
 
-Movie.create!({
-  movie_db_id: 9728;
-})
+Movie.create!(
+  movie_db_id: 9728,
+  list_id: friday_thirteen.id
+)
 
-Movie.create!({
-  movie_db_id: 138843;
-})
+Movie.create!(
+  movie_db_id: 138843,
+  list_id: friday_thirteen.id
+)
