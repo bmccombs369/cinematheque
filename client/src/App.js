15b453f5b2524d41d307bfa54912e74cb6e381dc
bmccombs369 <bmccombs369@gmail.com>
2018-08-15 13:02:52 -0400

    created a lists route in react router

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import IndividualMovie from './components/IndividualMovie';
+import ListsIndex from './components/ListsIndex';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/movie/:id' component={IndividualMovie} />
+          <Route exact path='/lists' component={ListsIndex} />
         </Switch>
       </Router>
     );
