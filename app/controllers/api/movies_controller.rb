fe51f5111a7f6f85fead4e392641ee4d307e2d41
bmccombs369 <bmccombs369@gmail.com>
2018-08-15 12:51:23 -0400

    created movies controller and index route is working

---
+++ app/controllers/api/movies_controller.rb
@@ -0,0 +1,28 @@
+class Api::MoviesController < ApplicationController
+  def index
+    @movies = List.find(params[:list_id]).movies
+    render json: @movies
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:movie_db_id)
+  end
+end
--- client/src/components/ListsIndex.js
+++ client/src/components/ListsIndex.js
@@ -10,13 +10,32 @@ class ListsIndex extends Component {
   }
 
   fetchLists = () => {
-    
+
   }
 
   render() {
     return (
       <div>
-        
+        <div>
+          <ul>
+            <li>
+              <h3>The Grasslands</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet.</p>
+            </li>
+            <li>
+              <h3>Paradise Found</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet.</p>
+            </li>
+            <li>
+              <h3>Smoke On The Water</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet.</p>
+            </li>
+            <li>
+              <h3>Headline</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet.</p>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
